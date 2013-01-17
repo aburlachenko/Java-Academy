a92b1d04acce65ecd757edc3579390979ac9f08e
Antonina <antonina.gubynets@gmail.com>
2013-01-18 00:37:22 +0200

    Refactored first homework

--- Sum.java
+++ Sum.java
@@ -1,7 +1,7 @@
 public class Sum {
 
     public static void main(String args[]) {
-		int sum = 0;
+        int sum = 0;
         for (String arg: args) {
             String[] parts = arg.split("[\\s\"]");
             for (String part: parts) {
